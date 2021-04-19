aaa0131d0a568186305093fc338c9601e34e959b
Rupak Lama <62438399+rupaklama@users.noreply.github.com>
2021-04-19 17:50:36 -0400

    Update repositoriesReducer.ts

--- src/reducers/repositoriesReducer.ts
+++ src/reducers/repositoriesReducer.ts
@@ -51,11 +51,11 @@ export const repositoriesReducer = (
   // We know with 100% certainty that all the 'action' satisfies the above interfaces
   switch (action.type) {
     case ActionType.SEARCH_REPOSITORIES:
-      return { ...state, loading: true };
+      return { ...state, loading: true, error: null, data: [] };
     case ActionType.SEARCH_REPOSITORIES_SUCCESS:
-      return { ...state, data: action.payload };
+      return { ...state, loading: false, error: null, data: action.payload };
     case ActionType.SEARCH_REPOSITORIES_ERROR:
-      return { ...state, error: action.payload };
+      return { ...state, loading: false, error: action.payload, data: [] };
 
     default:
       return state;
